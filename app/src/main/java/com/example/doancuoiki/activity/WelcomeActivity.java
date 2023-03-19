a9c5e811ac087ee8cbe9c980eb1e77bf0836cdb6
Quyen01022002 <20110305@student.hcmute.edu.vn>
2023-03-19 13:56:31 +0700

    upload

---
+++ app/src/main/java/com/example/doancuoiki/activity/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.doancuoiki.activity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.doancuoiki.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.welcome_activity);
+    }
+}
